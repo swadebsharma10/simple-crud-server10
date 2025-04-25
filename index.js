dddc07f6e8ae238b1109fc5615fd06cdf572cbbc
SwadebDeb sharma <swadebriva2012@gmail.com>
2025-04-26 01:18:33 +0600

    connected to the mongodb drivers

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
@@ -7,9 +8,39 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// CrudServer25
+// VMJrSa0XRkpXiabu
+
 app.get("/", (req, res) => {
   res.send("simple crud is running");
 });
+
+const uri =
+  "mongodb+srv://CrudServer25:VMJrSa0XRkpXiabu@cluster0.fikwith.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    //  await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
   console.log(`Simple crud is running  on port ${port}`);
