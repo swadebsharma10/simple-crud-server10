79776015477db82b84ba35710ababcd8f4741c41
SwadebDeb sharma <swadebriva2012@gmail.com>
2025-04-26 18:52:51 +0600

    create delete api and delete a data drom databse

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
@@ -43,6 +43,13 @@ async function run() {
     app.get('/users', async(req, res)=>{
       const cursor = usersCollection.find();
       const result = await cursor.toArray() ;
+      res.send(result)
+    })
+
+    app.delete('/users/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id : new ObjectId(id)};
+      const result = await usersCollection.deleteOne(query);
       res.send(result)
     })
 
