75f232742ac626b597c6b628eea5303ec025522e
SwadebDeb sharma <swadebriva2012@gmail.com>
2025-04-26 18:14:47 +0600

    insert single user to database and read data  form database

--- index.js
+++ index.js
@@ -30,7 +30,23 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
+
+    const usersCollection = client.db("crudDB").collection("users")
+   
+    app.post('/users', async(req, res)=>{
+      const user = req.body;
+      const result = await usersCollection.insertOne(user);
+      res.send(result)
+
+    })
+
+    app.get('/users', async(req, res)=>{
+      const cursor = usersCollection.find();
+      const result = await cursor.toArray() ;
+      res.send(result)
+    })
+
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
